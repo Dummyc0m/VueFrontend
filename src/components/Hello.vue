1c95f59c6666a967d93c957785583f3c5e6d901c
Dummyc0m <m.steve.jobs@me.com>
2017-01-08 17:08:08 -0500

    Demo Question Card change

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
-        <QuestionCard></QuestionCard>
+        <QuestionCard section="2" number="1" content="二三十年<latex>\int \left(x^2+ax-3\right)^2dx</latex>">
+
+        </QuestionCard>
         <CourseCard :id="-1"></CourseCard>
         <TreeMenu></TreeMenu>
         <CourseSection :edit="true"></CourseSection>
@@ -12,6 +14,8 @@
     import CourseCard from 'components/Course/CourseCard'
     import TreeMenu from 'components/TreeMenu/TreeMenu'
     import CourseSection from 'components/Course/Section/CourseSection'
+//    import MultipleSelectionQuestionCard from 'components/Questions/MultipleSelectionQuestionCard'
+
     export default {
         name: 'TestPage',
         data () {
