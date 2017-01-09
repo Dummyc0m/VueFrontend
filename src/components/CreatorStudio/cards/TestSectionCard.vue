5bf3e0ebf925c4054478d9a3fdaa66c2b58e766a
DEDZTBH <peiqial@outlook.com>
2017-01-09 14:23:06 -0500

    working on test section editing

--- src/components/CreatorStudio/cards/TestSectionCard.vue
+++ src/components/CreatorStudio/cards/TestSectionCard.vue
@@ -5,20 +5,25 @@
             <md-dialog-title style="min-width: 640px">编辑题目</md-dialog-title>
 
             <md-dialog-content v-if="editId > -1">
-                <md-input-container>
+                <md-input-container v-if="false">
                     <label>类型</label>
                     <md-select v-model="tempQuest.type">
                         <md-option value="MC" @click.native="changeType('MC')">单选题</md-option>
                         <md-option value="Text" @click.native="changeType('Text')">解答题</md-option>
                     </md-select>
                 </md-input-container>
+
                 <md-input-container>
                     <label>问题</label>
                     <md-input v-model="tempQuest.title"></md-input>
                 </md-input-container>
                 <md-input-container>
                     <label>答案</label>
-                    <md-input v-model="tempQuest.answer"></md-input>
+                    <md-input v-model="tempQuest.answer" v-if="tempQuest.type === 'Text'"></md-input>
+                    <md-select v-model="tempQuest.answer" v-else-if="tempQuest.type === 'MC'">
+                        <md-option value="" v-if="false"></md-option>
+                        <md-option v-for="(opt,index) in tempQuest.body" :value="index">{{opt}}</md-option>
+                    </md-select>
                 </md-input-container>
                 <md-input-container>
                     <label>分数</label>
@@ -28,20 +33,20 @@
                     <label>选项</label>
                     <md-table>
                         <md-table-body>
-                            <md-table-row v-for="(opt,index) in tempQuest.options" :class="{'active': index === editOption}">
+                            <md-table-row v-for="(opt,index) in tempQuest.body" :class="{'active': index === editOption}">
                                 <md-table-cell>{{opt}}</md-table-cell>
                                 <md-table-cell>
                                     <div style="font-size: large; float: right">
                                         <i class="fa fa-pencil" @click="editOption = (editOption > -1 ? -1 : index)" style="margin-right: .5em"></i>
-                                        <i class="fa fa-times" @click="tempQuest.options.splice(index,1)"></i>
+                                        <i class="fa fa-times" @click="rmSelect(index)"></i>
                                     </div>
                                 </md-table-cell>
                             </md-table-row>
                             <md-table-row>
-                                <md-table-cell @click.native="tempQuest.options.push('')">
+                                <md-table-cell @click.native="tempQuest.body.push('新选项')">
                                     <h3>添加选项</h3>
                                 </md-table-cell>
-                                <md-table-cell @click.native="tempQuest.options.push('')">
+                                <md-table-cell @click.native="tempQuest.body.push('新选项')">
                                 </md-table-cell>
                             </md-table-row>
                         </md-table-body>
@@ -49,22 +54,16 @@
                 </div>
                 <label v-else-if="tempQuest.type === 'Text'">描述</label>
 
-                <!--<vue-html5-editor></vue-html5-editor>-->
+                <vue-html5-editor v-if="tempQuest.type === 'Text'"
+                                  v-model="tempQuest.body"></vue-html5-editor>
 
-                <md-input-container v-if="tempQuest.type === 'Text'">
-                    <label>暂时用来代替富文本编辑器</label>
-                    <md-textarea v-model="tempQuest.body"></md-textarea>
-                </md-input-container>
-
-                <md-input-container v-else-if="tempQuest.type === 'MC' && editOption !== -1">
-                    <label>暂时用来代替富文本编辑器</label>
-                    <md-textarea v-model="tempQuest.options[editOption]"></md-textarea>
-                </md-input-container>
+                <vue-html5-editor v-else-if="tempQuest.type === 'MC' && editOption !== -1"
+                                  v-model="tempQuest.body[editOption]"></vue-html5-editor>
             </md-dialog-content>
 
             <md-dialog-actions>
                 <md-button class="md-primary" @click="finishEdit(false)">返回</md-button>
-                <md-button class="md-primary" @click="finishEdit(true)">保存</md-button>
+                <md-button class="md-primary" @click="finishEdit(true)" :disabled="!valid">保存</md-button>
             </md-dialog-actions>
         </md-dialog>
 
@@ -83,7 +82,7 @@
                                 <div v-if="quest.type === 'MC'">
                                     <h3>选项：</h3>
                                     <ul>
-                                        <li v-for="opt in quest.options">{{opt}}</li>
+                                        <li v-for="opt in quest.body">{{opt}}</li>
                                     </ul>
                                 </div>
                                 <div v-else-if="quest.type === 'Text'">
@@ -114,11 +113,39 @@
                 section: {
                     sectionName: '测试section',
                     questions: [
-                        {type: 'MC', title: '选择题：1+1', options: ['1', '2', '3', '10'], answer: '10', point: 2},
+                        {type: 'MC', title: '选择题：1+1', body: ['1', '2', '3', '10'], answer: 3, point: 2},
                         {type: 'Text', title: '解答题：苟', body: '大雾', answer: '苟富贵无相忘', point: 1}
                     ]
                 },
                 tempQuest: undefined
+            }
+        },
+        computed: {
+            valid () {
+                let valid = true
+                for (let key in this.tempQuest) {
+                    if (this.tempQuest.hasOwnProperty(key)) {
+                        if (this.tempQuest[key] === '') {
+//                            console.log('empty: ' + key)
+                            valid = false
+                            break
+                        } else if (this.tempQuest[key] instanceof Array) {
+                            if (this.tempQuest[key].length) {
+                                this.tempQuest[key].forEach(function (a) {
+                                    if (a === '') {
+//                                        console.log('empty option in: ' + a)
+                                        valid = false
+                                    }
+                                })
+                            } else {
+//                                console.log('empty array: ' + key)
+                                valid = false
+                                break
+                            }
+                        }
+                    }
+                }
+                return valid
             }
         },
         methods: {
@@ -127,13 +154,19 @@
                 this.tempQuest = this.deepcopy(this.section.questions[this.editId])
                 this.$refs['editDialog'].open()
             },
+            rmSelect (index) {
+                this.tempQuest.body.splice(index, 1)
+                this.tempQuest.answer = ''
+            },
             finishEdit (save) {
-                if (save) {
-                    this.section.questions[this.editId] = this.deepcopy(this.tempQuest)
+                if (this.valid || !save) {
+                    if (save) {
+                        this.section.questions[this.editId] = this.deepcopy(this.tempQuest)
+                    }
+                    this.tempQuest = undefined
+                    this.editId = -1
+                    this.$refs['editDialog'].close()
                 }
-                this.tempQuest = undefined
-                this.editId = -1
-                this.$refs['editDialog'].close()
             },
             deepcopy (obj) {
                 return JSON.parse(JSON.stringify(obj))
@@ -141,14 +174,13 @@
             changeType (cur) {
                 switch (cur) {
                     case 'MC':
-                        delete this.tempQuest.body
-                        this.tempQuest.options = []
+                        this.tempQuest.body = []
                         break
                     case 'Text':
-                        delete this.tempQuest.options
                         this.tempQuest.body = ''
                         break
                 }
+                this.answer = ''
             }
         },
         components: {
